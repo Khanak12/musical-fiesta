e0301f0af14aa571820413414a2e77a6975abdbc
Khanak12 <khanakjoshi2012@gmail.com>
2024-12-07 15:25:45 -0500

    Add files via upload

---
+++ main.35.py
@@ -0,0 +1,15 @@
+class a:
+    def __init__(self):
+        self.k=""
+        
+    def getsting(self):
+        self.k=input("Please enter a string ")
+    def getstring1(self):
+        print(self.k.upper())
+        
+obj=a()
+obj.getsting()
+obj.getstring1()
+
+
+        
